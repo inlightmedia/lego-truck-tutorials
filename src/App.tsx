053cf8bccf957485d25e5f454eb016656893b8ff
Josh Dyck <josh.dyck@inlightmedia.ca>
2024-09-29 15:43:03 -0400

    feat: remove unused variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
 import ImageGallery from "./components/ImageGallery"
 
-const stats = [
-  { id: 1, name: 'Creators on the platform', value: '8,000+' },
-  { id: 2, name: 'Flat platform fee', value: '3%' },
-  { id: 3, name: 'Uptime guarantee', value: '99.9%' },
-  { id: 4, name: 'Paid out to creators', value: '$70M' },
-]
-
-const cardData = [
-  { name: 'Total Subscribers', stat: '71,897' },
-  { name: 'Avg. Open Rate', stat: '58.16%' },
-  { name: 'Avg. Click Rate', stat: '24.57%' },
-]
-
 function App() {
   return (
     <>
